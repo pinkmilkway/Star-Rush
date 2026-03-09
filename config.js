2aa2ef7dbe69fa27dbee24443041e714e1d3a784
audrey-emilly <audrey.s@aluno.ifsc.edu.br>
2026-03-09 14:27:14 +0000

    Iniciar tutorial Phaser

---
+++ config.js
@@ -0,0 +1,8 @@
+ var config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        parent: "game-container",
+    };
+
+    export default config;
---
+++ game.js
@@ -0,0 +1,15 @@
+import config from "./config.js";
+import scene0 from "./scene0.js";
+
+class Game extends Phaser.Game { 
+    constructor () { 
+        super(config);
+
+        this.scene.add("scene0,scene0")
+        this.scene.start("scene0");
+    }
+ }  
+
+window.onload = () => { 
+    window.game  new Game(); 
+};
