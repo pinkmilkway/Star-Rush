a2e256167bbd4147682390ec534cf16cd7941c21
Iza-Beli <izabeli.k@aluno.ifsc.edu.br>
2026-03-10 12:42:41 +0000

    iniciar tutorial phaser

--- config.js
+++ config.js
@@ -1,8 +1,19 @@
- var config = {
-        type: Phaser.AUTO,
-        width: 800,
-        height: 600,
-        parent: "game-container",
-    };
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: "game-container",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300},
+      debug: true
+    }
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+};
 
-    export default config;
+export default config;
--- scene0.js
+++ scene0.js
@@ -1,33 +1,33 @@
 class scene0 extends Phaser.Scene {
-constructor() { 
-   super("scene0")
+  constructor() {
+    super("scene0");
+  
+    this.playes;
+    this.stars;
+    this.bombs;
+    this.platforms;
+    this.cursors;
+    this.score = 0;
+    this.gameOver = false;
+    this.scoreText;
+  }
+  
+  preload() {
+    this.load.image("sky", "assets/sky.png");
+    this.load.image("ground", "assets/platform.png");
+    this.load.image("star", "assets/star.png");
+    this.load.image("bomb", "assets/bomb.png");
+    this.load.spritesheet("dude", "assets/dude.png", {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
+  }
+
+  create() {
+    this.add.image(400, 300, "sky");
+    
+  }
+  update() {}
 }
 
-preload ()
-    {
-        this.load.image('sky', 'assets/sky.png');
-        this.load.image('ground', 'assets/platform.png');
-        this.load.image('star', 'assets/star.png');
-        this.load.image('bomb', 'assets/bomb.png');
-        this.load.spritesheet('dude', 'assets/dude.png', 
-            { 
-                frameWidth: 32,
-                frameHeight: 48
-             });
-    }
-
-    create() { }
-    update() { }
-}
-export default scene0; 
-
-
-
-
-
-
-
-
- }
-
-
+export default scene0;
